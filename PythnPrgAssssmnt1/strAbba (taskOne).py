45382d0fd83a7e64f18e1e8b12b11915fa3b3bdb
MegRCooper <58695342+MegRCooper@users.noreply.github.com>
2020-09-29 16:29:23 +0100

    Update strAbba (taskOne).py
    Removed the comment explaining task - refer to READ.ME

--- PythnPrgAssssmnt1/strAbba (taskOne).py
+++ PythnPrgAssssmnt1/strAbba (taskOne).py
@@ -1,13 +1,6 @@
-''' 
-Step 1) Initalise the function (NO arguments):
-Step 2) Take users first input [A] (Str):
-Step 3) Take users second input [B] (Str):
-Step 4) return the String in [ABBA] format:
-'''
-
-def strAbba (): # Step One
-    userInpA = input("Please enter your first word ") #Step Two
-    userInpB = input("Please enter your second word ") #Step Three
-    return(userInpA + userInpB * 2 + userInpA) #Step Four
+def strAbba (): 
+    userInpA = input("Please enter your first word ") 
+    userInpB = input("Please enter your second word ") 
+    return(userInpA + userInpB * 2 + userInpA) 
 #print(strAbba())
-strAbba() # Calls Function
+strAbba() 
