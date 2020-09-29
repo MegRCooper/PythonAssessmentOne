5ef76a0ab0096404aba9bcb3d3126113d5e8c1b6
MegRCooper <58695342+MegRCooper@users.noreply.github.com>
2020-09-29 16:28:13 +0100

    Update pigLatin (Task Four).py
    Removed the comment explaining task - refer to READ.ME

--- PythnPrgAssssmnt1/pigLatin (Task Four).py
+++ PythnPrgAssssmnt1/pigLatin (Task Four).py
@@ -1,21 +1,7 @@
-'''
-Step 1) Define function ()
-Step 2) User input (String)
-Step 3) Converstion into Piglatin.
-      Step 3.1: Split the users inpt into an arr
-      Step 3.2: Complete the translation of the word for all words (for loop)
-         Step 3.2a: 1st letter moved to the end of the word.
-         Step 3.2b: followed by the letters ay
-         Here is an example: This assignment is really bizarre
-         becomes histay ssignmentaay siay eallyray izarrebay  
-Step 4) Returns Value as a string
-'''
-
-usersStr = input("Please enter the text you want translated into pig Latin: ") #Step 2
-def pigLatin (usersStr): #Step 1
-    usersStrArr = usersStr.split() #Step 3.2 
+usersStr = input("Please enter the text you want translated into pig Latin: ") 
+def pigLatin (usersStr): 
+    usersStrArr = usersStr.split() 
     pigLatinStr = str()
-    for char in usersStrArr: #Step 3.2
+    for char in usersStrArr: 
         pigLatinStr = pigLatinStr + ' ' + str((char[1:] + char[0] + "ay")).lower() #Uses String slicing to move the chars.
-    return (pigLatinStr) #step 4
-# print(pigLatin(usersStr))
+    return (pigLatinStr) 
