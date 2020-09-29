612f4050d4d849882adcededac86b9ea0cfd56d8
MegRCooper <58695342+MegRCooper@users.noreply.github.com>
2020-09-29 16:17:49 +0100

    Update compareAges (TaskTwo).py
    Removed the comment explaining task - refer to READ.ME

--- PythnPrgAssssmnt1/compareAges (TaskTwo).py
+++ PythnPrgAssssmnt1/compareAges (TaskTwo).py
@@ -1,14 +1,3 @@
-''' 
-Step 1) Initialize function (NO arguements):
-Step 2) Take users age (int):
-Step 3) Set Constant as myAge (19):
-Step 4) Three comparisons:
-   Step 4.1: You are younger than ...
-   Step 4.2: You are the same age ...
-   Step 4.3: You are older than ... 
-Step 5) Output:
-'''
-
 def compareAges (): #Step One
     usersAge = int(input("Please enter your age in Years only: ")) #Step Two
     MYAGE = 19 #Step 3
