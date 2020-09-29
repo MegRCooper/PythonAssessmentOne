36879fb17838d7f5271d363d311649fc516730d8
MegRCooper <58695342+MegRCooper@users.noreply.github.com>
2020-09-29 16:19:08 +0100

    Update guessingGame (TaskThree).py

--- PythnPrgAssssmnt1/guessingGame (TaskThree).py
+++ PythnPrgAssssmnt1/guessingGame (TaskThree).py
@@ -1,19 +1,19 @@
-import random #Step 2.1   
-def guessingGame (): #Step 1
-    guessNum = 1 # Sets up the counter to keep score of the users num of guesses
-    randNum = random.randint (1,100) #Step 2.2
-    usersGuess = int(input("What is your numberical guess (Whole numbers only) ")) #Step 3
-    while usersGuess != randNum: # Step 4 (Set up of while loop only)
-        if usersGuess > randNum: # Step 5.1
+import random   
+def guessingGame (): 
+    guessNum = 1 
+    randNum = random.randint (1,100) 
+    usersGuess = int(input("What is your numberical guess (Whole numbers only) ")) 
+    while usersGuess != randNum: 
+        if usersGuess > randNum:
             print ("Your guess is too big.")
             usersGuess = int(input("Please guess again: "))
             guessNum += 1 
-        elif usersGuess < randNum: #Step 5.2 
+        elif usersGuess < randNum: 
             print ("Your guess is too small.")
             usersGuess = int(input("Please guess again: "))
             guessNum += 1 
-        else: #Step 5.3
+        else: 
             guessNum += 1 
-    print ("You guessed correctly. \nYou guessed in " + str(guessNum) + " guesses.") #Step 6
+    print ("You guessed correctly. \nYou guessed in " + str(guessNum) + " guesses.") 
 
-guessingGame() #Calls Func. 
+guessingGame()
