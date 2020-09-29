e81af7d0a9a4f9998ec8c15b3f263cec2f6388bf
MegRCooper <58695342+MegRCooper@users.noreply.github.com>
2020-09-29 16:26:12 +0100

    Update compareAges (TaskTwo).py

--- PythnPrgAssssmnt1/compareAges (TaskTwo).py
+++ PythnPrgAssssmnt1/compareAges (TaskTwo).py
@@ -1,12 +1,12 @@
-def compareAges (): #Step One
-    usersAge = int(input("Please enter your age in Years only: ")) #Step Two
-    MYAGE = 19 #Step 3
-    if usersAge > MYAGE: #Step 4.3
+def compareAges (): 
+    usersAge = int(input("Please enter your age in Years only: ")) 
+    MYAGE = 19 
+    if usersAge > MYAGE: 
         ageDiff = usersAge - MYAGE
         print ("You are older than me by " + str(ageDiff) + " year(s).")
-    elif usersAge == MYAGE: #Step 4.2
+    elif usersAge == MYAGE: 
         print ("You are the same age as me.")
     else:
-        ageDiff = MYAGE - usersAge #Step 4.1
+        ageDiff = MYAGE - usersAge 
         print ("You are younger than me by " + str(ageDiff) + " year(s).")
-compareAges() # Call func
+compareAges() 
