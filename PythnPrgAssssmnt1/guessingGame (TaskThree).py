afb9a719bdf1834aa10b5bc90fff1b0e0cfad1d2
MegRCooper <58695342+MegRCooper@users.noreply.github.com>
2020-09-29 16:18:14 +0100

    Update guessingGame (TaskThree).py
    Removed the comment explaining task - refer to READ.ME

--- PythnPrgAssssmnt1/guessingGame (TaskThree).py
+++ PythnPrgAssssmnt1/guessingGame (TaskThree).py
@@ -1,20 +1,3 @@
-''' 
-Step 1) Initialize function (NO arguements):
-Step 2) Generate random number (1,100 inclusive):
-   Step 2.1) Import the random Module:
-   Step 2.2) Generate the random number: 
-Step 3) Take users input of the guess (int):
-Step 4) while loop till the correct number is guessed + Step 5:
-Step 5) If statement in order to provide help (Comparison of numbers) :
-   Step 5.1: Answer was too big (Output)
-     Step 5.1.a) Add count to keep score
-   Step 5.2: Answer was too small (Output) 
-     Step 5.2.a) Add count to keep score
-     Step 5.2.b) Ask the user to reenter a guess
-   Step 5.3: Answer was correct - with the num of guesses (Output) 
-     Step 5.3.a) Add count to keep score
-Step 6) Output the users number of guesses
-'''
 import random #Step 2.1   
 def guessingGame (): #Step 1
     guessNum = 1 # Sets up the counter to keep score of the users num of guesses
